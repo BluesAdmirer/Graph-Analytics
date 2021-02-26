20fb3199c5b0b1e1a95fb3145e58b1879116b82f
Dev Lathiya <53378552+deus09@users.noreply.github.com>
2021-02-26 20:18:29 +0530

    Update kernels.cuh

--- kernels.cuh
+++ kernels.cuh
@@ -1,15 +1,22 @@
 __global__ void kernel(int *cstart, int *cend, int *cmemsz, int *cmember, int *crcw, 
 	double *cinitial, double *crank, int *rcwgraph, int *outdeg, int *corder, int *ctemp, int *ctempg)
 {
+	// cstart = par[i]
+	// cend = par[i+1]
+	// w = from cstart to cend 
 	int w = blockIdx.x + (*cstart);
 	if(w < (*cend)){
+		// size = number of nodes in SCC numbered w
 		int size = cmemsz[corder[w]];
+		// j index of node in SCC w
 		int j = threadIdx.x;
 		if(j < size){
 			int node = cmember[ctemp[corder[w]]+j];
 			int k = threadIdx.y;
+			// kth node in rcw graph of node
 			int size1 = crcw[node];
 			if(k < size1){
+				// update cinitial
 				atomicAdd(&cinitial[node], 0.85*crank[rcwgraph[ctempg[node]+k]]/outdeg[rcwgraph[ctempg[node]+k]]);
 			}
 		}
@@ -25,6 +32,7 @@ __global__ void kernel1(int *cn, int *csize, int *cmem, int *cgraph,
 		int j = threadIdx.x;
 		if(j < size){
 			int node = cgraph[ctemp[w]+j];
+			// parent node ki rank value and wo node ki degree
 			atomicAdd(&ccurr[w], crank[cparent[node]]/coutdeg[node]);
 		}
 	}
@@ -34,11 +42,13 @@ __global__ void kernel2(int *cn, int *csize, int *cmem, int *cgraph,
 							int *ctemp, double *ccurr, double *crank, int *coutdeg, int *cparent, int *cmarked)
 {
 	int w = blockIdx.x;
+	// cmarked == dead node or not
 	if(w < (*cn) && cmarked[w]==0){
 		int size = csize[w];
 		int j = threadIdx.x;
 		if(j < size){
 			int node = cgraph[ctemp[w]+j];
+			// parent node ki rank value and wo node ki degree
 			atomicAdd(&ccurr[w], crank[cparent[node]]/coutdeg[node]);
 		}
 	}
@@ -47,6 +57,7 @@ __global__ void kernel2(int *cn, int *csize, int *cmem, int *cgraph,
 __global__ void kernel3(int *cn, int *csize, int *cmem, int *cgraph, 
 							int *ctemp, double *ccurr, double *crank, int *coutdeg)
 {
+	// simple code
 	int w = blockIdx.x;
 	if(w < (*cn)){
 		int size = csize[w];
@@ -62,6 +73,7 @@ __global__ void kernel4(int *cn, int *csize, int *cmem, int *cgraph,
 							int *ctemp, double *ccurr, double *crank, int *coutdeg, int *cmarked)
 {
 	int w = blockIdx.x;
+	// only dead node combination
 	if(w < (*cn) && cmarked[w]==0){
 		int size = csize[w];
 		int j = threadIdx.x;
