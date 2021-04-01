c38543566f201e4fae08dcaa696954aba8bbf285
Dev Lathiya <53378552+deus09@users.noreply.github.com>
2021-04-01 14:28:23 +0530

    Update kernels.cuh

--- kernels.cuh
+++ kernels.cuh
@@ -1,26 +1,65 @@
+// cross edge (Edge which have both end nodes are in different Components) contribution
+/*
+	Example,
+		Components (levelwise),
+			0 (level0)
+			1 2 (level1)
+			3 4 5 (level2)
+		Component edges
+			0 -> 1
+			0 -> 2
+			1 -> 3
+			1 -> 4
+			2 -> 5
+			1 -> 5
+	For level1, cstart = 1
+		    cend = 3
+	let us say w = 2
+	for component 2, for all the nodes which have incoming edges from component 0,
+	we will initialize that nodes with contribution of the node from which edge is coming
+*/
 __global__ void kerneltest(long long *cstart, long long *cend, long long *cmemsz, long long *cmember, long long *crcw, 
 	double *cinitial, double *crank, long long *rcwgraph, long long *outdeg, long long *corder, long long *ctemp, long long *ctempg)
 {
+	// w = Component number
 	long long w = blockIdx.z*blockDim.z + threadIdx.z + (*cstart);
+	// num_threads_z = max threads in z dimension
 	long long num_threads_z = blockDim.z * gridDim.z;
-	
+	// w is on the same level i.e. w belongs to [cstart, cend)
 	for(;w < (*cend);w+=num_threads_z)
 	{
+		// size = number of nodes in component w
 		long long size = cmemsz[w];
+		// j is the index of node
 		long long j = blockIdx.x*blockDim.x+threadIdx.x;
+		// num_thread_x = max threads in x dimension
 		long long num_threads_x = blockDim.x * gridDim.x;
 		for(;j<size;j+=num_threads_x){
+			// node values of node
+			// ctemp[w] = number of nodes skipped to get to the list of members of component w
+			// ctemp[w] + j = jth node index of members of component w
 			long long node = cmember[ctemp[w]+j];
+			// k is the index of node from which node have incoming edge in the graph rcwgrah
 			long long k = blockIdx.y*blockDim.y+threadIdx.y;
+			// size1 = size of node's cross edge list 
 			long long size1 = crcw[node];
+			// num_threads_y = max threads in y dimension
 			long long num_threads_y = blockDim.y * gridDim.y;
 			for(;k<size1;k+=num_threads_y){
+				// there is an cross edge (u -> node)
+				// ctempg[node] = number of nodes skipped to get to the edge list of the node
+				// u = rcwgraph[ctempg[node]+k] => kth node in rcw list of the node
+				// node u's pagerank already been calculated so its contribution is fixed and it is stored in initial
 				atomicAdd(&cinitial[node], 0.85*crank[rcwgraph[ctempg[node]+k]]/outdeg[rcwgraph[ctempg[node]+k]]);
 			}
 		}
 	}
 }
 
+
+// This function works same as above kerneltest function
+// One thing is changed
+// Component is fixed (cstart/w)
 __global__ void kerneltest1(long long *cstart, long long *cend, long long *cmemsz, long long *cmember, long long *crcw, 
 	double *cinitial, double *crank, long long *rcwgraph, long long *outdeg, long long *corder, long long *ctemp, long long *ctempg)
 {
